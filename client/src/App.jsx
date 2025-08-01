cf74244b9fe1f4bd26a040e013f5a91b18e74846
Võ Minh Tú <52200130@student.tdtu.edu.vn>
2025-08-01 09:38:52 +0700

    init projetc

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { assets } from "./assets/assets";
+const App = () => {
+  return (
+    <div className="bg-amber-200">
+      <img src={assets.logo} alt="" />
+      hello Thắng
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/assets/assets.js
@@ -0,0 +1,21 @@
+import logo from "./logo.png";
+
+export const assets = { logo };
+
+export const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 10,
+    image: "product1.jpg",
+  },
+];
+
+const users = [
+  {
+    id: 1,
+    name: "User 1",
+    email: "user1@example.com",
+    password: "password1",
+  },
+];
